8df4e15bfabae37de02ac8b5d3cb1e2f498afae6
Michael Winkelmann <michael@winkelmann.site>
2025-10-25 13:45:47 +0200

    builtin geo2d::Line: Use proc macro

--- builtin/geo2d/line.rs
+++ builtin/geo2d/line.rs
@@ -1,63 +1,30 @@
 // Copyright © 2025 The µcad authors <info@ucad.xyz>
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
+use microcad_builtin_proc_macros::BuiltinPrimitive2D;
 use microcad_core::*;
 use microcad_lang::{builtin::*, render::*};
 
 /// Built-in line primitive.
-pub struct Line(geo2d::Line);
-
-impl Line {
-    pub fn new(x0: Scalar, y0: Scalar, x1: Scalar, y1: Scalar) -> Self {
-        use geo::coord;
-
-        Self(geo2d::Line(
-            coord! {x: x0, y: y0}.into(),
-            coord! {x: x1, y: y1}.into(),
-        ))
-    }
+#[derive(BuiltinPrimitive2D)]
+pub struct Line {
+    x0: Scalar,
+    y0: Scalar,
+    x1: Scalar,
+    y1: Scalar,
 }
 
 impl Render<Geometry2D> for Line {
     fn render(&self, _: &RenderResolution) -> Geometry2D {
-        Geometry2D::Line(self.0.clone())
+        Geometry2D::Line(geo2d::Line(
+            (self.x0, self.y0).into(),
+            (self.x1, self.y1).into(),
+        ))
     }
 }
 
 impl RenderWithContext<Geometry2DOutput> for Line {
     fn render_with_context(&self, context: &mut RenderContext) -> RenderResult<Geometry2DOutput> {
         context.update_2d(|context, _| Ok(self.render(&context.current_resolution())))
-    }
-}
-
-impl BuiltinWorkbenchDefinition for Line {
-    fn id() -> &'static str {
-        "Line"
-    }
-
-    fn kind() -> BuiltinWorkbenchKind {
-        BuiltinWorkbenchKind::Primitive2D
-    }
-
-    fn workpiece_function() -> &'static BuiltinWorkpieceFn {
-        &|args| {
-            Ok(BuiltinWorkpieceOutput::Primitive2D(Box::new(Line::new(
-                args.get("x0"),
-                args.get("y0"),
-                args.get("x1"),
-                args.get("y1"),
-            ))))
-        }
-    }
-
-    fn parameters() -> ParameterValueList {
-        [
-            parameter!(x0: Scalar),
-            parameter!(y0: Scalar),
-            parameter!(x1: Scalar),
-            parameter!(y1: Scalar),
-        ]
-        .into_iter()
-        .collect()
     }
 }
