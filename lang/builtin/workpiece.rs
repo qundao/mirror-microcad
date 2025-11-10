8931b5311279311cf8de8e27d5961fe242e13e08
Patrick Hoffmann <p.hoffmann@berlin.de>
2025-11-10 08:28:58 +0100

    builtin: separated code of Builtin from BuiltinWorkpiece

--- lang/builtin/workpiece.rs
+++ lang/builtin/workpiece.rs
@@ -6,75 +6,9 @@
 use custom_debug::Debug;
 use strum::Display;
 
-use crate::{eval::*, model::*, render::*, resolve::*, src_ref::*, syntax::*, value::*};
-
-/// Builtin function type
-pub type BuiltinFn =
-    dyn Fn(&ParameterValueList, &ArgumentValueList, &mut EvalContext) -> EvalResult<Value>;
-
-/// Builtin function struct
-#[derive(Clone)]
-pub struct Builtin {
-    /// Name of the builtin function
-    pub id: Identifier,
-
-    /// Optional parameter value list to check the builtin signature.
-    pub parameters: ParameterValueList,
-
-    /// Built-in kind.
-    pub kind: BuiltinKind,
-
-    /// Functor to evaluate this function
-    pub f: &'static BuiltinFn,
-}
-
-/// Kind of the built-in.
-#[derive(Clone)]
-pub enum BuiltinKind {
-    /// A built-in function: `fn ()`.
-    Function,
-    /// A built-in workpiece: operation, transform, sketch, part.
-    Workbench(BuiltinWorkbenchKind),
-}
-
-impl std::fmt::Display for BuiltinKind {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            BuiltinKind::Function => write!(f, "Function"),
-            BuiltinKind::Workbench(kind) => write!(f, "{kind}"),
-        }
-    }
-}
-
-impl std::fmt::Debug for Builtin {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        write!(f, "__builtin::{}", &self.id)
-    }
-}
-
-impl Builtin {
-    /// Return identifier.
-    pub fn id(&self) -> Identifier {
-        self.id.clone()
-    }
-}
-
-impl CallTrait for Builtin {
-    /// Call builtin function with given parameter.
-    ///
-    /// # Arguments
-    /// - `args`: Function arguments.
-    /// - `context`: Execution context.
-    fn call(&self, args: &ArgumentValueList, context: &mut EvalContext) -> EvalResult<Value> {
-        (self.f)(&self.parameters, args, context)
-    }
-}
-
-impl Doc for Builtin {
-    fn doc(&self) -> Option<DocBlock> {
-        todo!()
-    }
-}
+use crate::{
+    builtin::*, eval::*, model::*, render::*, resolve::*, src_ref::*, syntax::*, value::*,
+};
 
 /// The kind of the built-in workbench determines its output.
 #[derive(Debug, Clone, Display, PartialEq)]
