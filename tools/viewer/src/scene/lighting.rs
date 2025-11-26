09386e9dc627a8a046224fca28d4178064daeb15
Michael Winkelmann <michael@winkelmann.site>
2025-11-26 16:25:46 +0100

    Improve Lighting

--- tools/viewer/src/scene/lighting.rs
+++ tools/viewer/src/scene/lighting.rs
@@ -7,17 +7,17 @@ use bevy::{
         system::{Commands, ResMut},
     },
     math::Vec3,
-    pbr::{DirectionalLight, light_consts},
+    pbr::{DirectionalLight, PointLight, light_consts},
     transform::components::Transform,
 };
 
+use crate::ToBevy;
+
 /// Setup lights
 pub fn spawn_lights(mut commands: Commands, mut state: ResMut<crate::state::State>) {
-    // Despawn all light entities to remove them from the scene
     for entity in &state.scene.light_entities {
         commands.entity(*entity).despawn();
     }
-
     let mut entities: Vec<Entity> = Vec::new();
     let radius = state.scene.radius;
 
@@ -25,7 +25,7 @@ pub fn spawn_lights(mut commands: Commands, mut state: ResMut<crate::state::Stat
         commands
             .spawn((
                 DirectionalLight {
-                    illuminance: light_consts::lux::AMBIENT_DAYLIGHT,
+                    illuminance: light_consts::lux::OVERCAST_DAY,
                     ..Default::default()
                 },
                 Transform::from_xyz(2.0 * radius, -radius, 3.0 * radius)
@@ -37,11 +37,39 @@ pub fn spawn_lights(mut commands: Commands, mut state: ResMut<crate::state::Stat
         commands
             .spawn((
                 DirectionalLight {
-                    illuminance: light_consts::lux::AMBIENT_DAYLIGHT,
+                    illuminance: light_consts::lux::OVERCAST_DAY,
                     ..Default::default()
                 },
                 Transform::from_xyz(-2.0 * radius, radius, -3.0 * radius)
                     .looking_at(Vec3::ZERO, Vec3::Z),
+            ))
+            .id(),
+    );
+    entities.push(
+        commands
+            .spawn((
+                PointLight {
+                    color: state.config.theme.brighter.to_bevy(),
+                    range: radius * 100.0,
+                    radius: radius / 10.0,
+                    intensity: 500000000.0,
+                    ..Default::default()
+                },
+                Transform::from_xyz(2.0 * radius, -2.0 * radius, 2.0 * radius),
+            ))
+            .id(),
+    );
+    entities.push(
+        commands
+            .spawn((
+                PointLight {
+                    color: state.config.theme.signal.to_bevy(),
+                    range: radius * 100.0,
+                    radius: radius / 10.0,
+                    intensity: 10000000.0,
+                    ..Default::default()
+                },
+                Transform::from_xyz(-2.0 * radius, 2.0 * radius, -2.0 * radius),
             ))
             .id(),
     );
