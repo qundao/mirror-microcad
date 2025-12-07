f7c6e101ae49952569479864b1f615a5758333ae
Michael Winkelmann <michael@winkelmann.site>
2025-12-07 13:28:40 +0100

    Manual test for show/hide

--- crates/viewer-ipc/manual_test.rs
+++ crates/viewer-ipc/manual_test.rs
@@ -2,32 +2,43 @@
 
 use microcad_viewer_ipc::*;
 
+fn prompt_for_confirmation(prompt: &str) -> anyhow::Result<bool> {
+    loop {
+        println!("{} (y/n)", prompt);
+        let mut input = String::new();
+        std::io::stdin().read_line(&mut input)?;
+        match input.trim().to_lowercase().as_str() {
+            "y" => return Ok(true),
+            "n" => return Ok(false),
+            _ => println!("Please enter 'y' or 'n'."),
+        }
+    }
+}
+
 /// Test show hide
 fn test_show_hide_window() -> anyhow::Result<()> {
     env_logger::init();
     let search_paths = vec![std::env::current_dir()?];
     let viewer = ViewerProcessInterface::run(&search_paths, false); // Start hidden
 
-    // Show window
-    viewer.send_request(ViewerRequest::Show)?;
+    let mut cycle = 0;
 
-    // Manually verify the window appears.
+    loop {
+        log::info!("Sending 'Show' request...");
+        viewer.send_request(ViewerRequest::Show)?;
+        if !prompt_for_confirmation("Is the window visible?")? {
+            return Err(anyhow::anyhow!("Window did not appear as expected."));
+        }
 
-    // Wait for user confirmation
-    println!("Press Enter after verifying the window is visible...");
-    let mut input = String::new();
-    std::io::stdin().read_line(&mut input)?;
+        log::info!("Sending 'Hide' request...");
+        viewer.send_request(ViewerRequest::Hide)?;
+        if !prompt_for_confirmation("Is the window hidden?")? {
+            return Err(anyhow::anyhow!("Window did not hide as expected."));
+        }
+        cycle += 1;
 
-    // Hide window
-    viewer.send_request(ViewerRequest::Hide)?;
-
-    // Manually verify the window disappears.
-
-    // Wait for user confirmation
-    println!("Press Enter after verifying the window is visible...");
-    let mut input = String::new();
-    std::io::stdin().read_line(&mut input)?;
-    Ok(())
+        log::info!("Show/Hide Cycle #{cycle}")
+    }
 }
 
 fn main() -> anyhow::Result<()> {
