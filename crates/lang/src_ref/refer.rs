9f3cf02ab1a36a62cd112db3679e8ca7d8915037
Robin Appelman <robin@icewind.nl>
2025-12-13 18:13:46 +0100

    add conversion from Refer to SourceSpan

--- crates/lang/src_ref/refer.rs
+++ crates/lang/src_ref/refer.rs
@@ -77,3 +77,15 @@ impl<T: std::iter::IntoIterator> std::iter::IntoIterator for Refer<T> {
         self.value.into_iter()
     }
 }
+
+impl<T> From<Refer<T>> for SourceSpan {
+    fn from(value: Refer<T>) -> Self {
+        value.src_ref.as_miette_span().unwrap()
+    }
+}
+
+impl<T> From<&Refer<T>> for SourceSpan {
+    fn from(value: &Refer<T>) -> Self {
+        value.src_ref.as_miette_span().unwrap()
+    }
+}
