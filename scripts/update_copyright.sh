6ee8be81e8d3b0151f4612c63ffe0671f43b1aa5
Patrick Hoffmann <p.hoffmann@berlin.de>
2025-12-14 20:35:48 +0100

    tools: move parameters into update_copyright.sh

--- scripts/update_copyright.sh
+++ scripts/update_copyright.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-cargo run --bin update_copyright
+cargo run --bin update_copyright -- . \
+    -S rs \
+    -S pest \
+    -S slint \
+    -S wgsl \
+    -S µcad \
+    -H toml \
+    -e "./target/*" \
+    -e "./tests/*.µcad" \
+    -e "./crates/cli/examples/*.µcad" \
+    -e "./thirdparty/*"
