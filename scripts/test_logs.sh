288d4612d1c8cf3f9642ab1d7d650298df1cedaf
Patrick Hoffmann <p.hoffmann@berlin.de>
2026-02-07 09:23:58 +0100

    scripts: update test_logs.sh

--- scripts/test_logs.sh
+++ scripts/test_logs.sh
@@ -1,8 +1,10 @@
-#/bin/bash
-
-find -name "*.log" | grep "doc/" | while read -r file; do
-    echo
-    echo "========= $file ========="
-    echo
-    cat "$file"
+#!/bin/bash
+rm test-errors.log
+find -name "*.log" | grep -E "doc/|book/" | while read -r file; do
+    if grep -q "error" "$file" || grep -q "warning" "$file"; then
+        echo >> test-errors.log
+        echo "========= $file =========" >> test-errors.log
+        echo >> test-errors.log
+        cat "$file" >> test-errors.log
+    fi
 done
