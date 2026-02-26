bdd75fd1b872b2bbfa019f07cde4bd32c03003c8
Michael Winkelmann <michael@winkelmann.site>
2026-02-26 18:03:12 +0100

    string: Use proc macro

--- crates/builtin/string.rs
+++ crates/builtin/string.rs
@@ -3,31 +3,26 @@
 
 //! µcad builtin string functions.
 
+use microcad_builtin_proc_macros::{builtin_fn, builtin_mod};
 use microcad_lang::{diag::PushDiag, eval::EvalError, resolve::Symbol, value::Value};
 
 /// Module for built-in string functions.
-pub fn string() -> Symbol {
-    crate::ModuleBuilder::new("string").symbol(count()).build()
+#[builtin_mod]
+pub fn string() {
+    [count];
 }
 
 /// Return the count of characters in a string.
-fn count() -> Symbol {
-    Symbol::new_builtin_fn(
-        "count",
-        [].into_iter(),
-        &|_params, args, ctx| {
-            let arg = args.get_single()?;
-            Ok(match &arg.1.value {
-                Value::String(s) => Value::Integer(s.chars().count() as i64),
-                _ => {
-                    ctx.error(
-                        arg.1,
-                        EvalError::BuiltinError("Value is not a string.".into()),
-                    )?;
-                    Value::None
-                }
-            })
-        },
-        None,
-    )
+#[builtin_fn]
+fn count() {
+    Ok(match &arg.1.value {
+        Value::String(s) => Value::Integer(s.chars().count() as i64),
+        _ => {
+            ctx.error(
+                arg.1,
+                EvalError::BuiltinError("Value is not a string.".into()),
+            )?;
+            Value::None
+        }
+    })
 }
