8d1a800697ac7fcf0026621cbe7bb196689686cc
Michael Winkelmann <michael@winkelmann.site>
2026-02-26 19:31:45 +0100

    Better builtin mod

--- crates/builtin/debug/mod.rs
+++ crates/builtin/debug/mod.rs
@@ -1,15 +1,188 @@
 // Copyright © 2025-2026 The µcad authors <info@microcad.xyz>
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-mod assert;
-
-pub use assert::*;
-
 use microcad_builtin_proc_macros::builtin_mod;
-use microcad_lang::resolve::*;
 
 /// Module for built-in debugging.
 #[builtin_mod]
-pub fn debug() {
-    [assert, assert_eq, assert_valid, assert_invalid]
+pub mod debug {
+    use microcad_lang::builtin::ValueAccess;
+    use microcad_lang::diag::PushDiag;
+    use microcad_lang::eval::ArgumentMatch;
+    use microcad_lang::eval::EvalError;
+    use microcad_lang::parameter;
+    use microcad_lang::resolve::Symbol;
+    use microcad_lang::value::Target;
+    use microcad_lang::value::Value;
+
+    pub fn assert() -> Symbol {
+        Symbol::new_builtin_fn(
+            "assert",
+            [
+                parameter!(v : Bool),                        // Parameter with any type
+                parameter!(message: String = String::new()), // Optional message
+            ]
+            .into_iter(),
+            &|params, args, context| {
+                match ArgumentMatch::find_multi_match(args, params) {
+                    Ok(multi_args) => {
+                        for a in multi_args.args {
+                            let v: bool = a.get("v");
+                            if !v {
+                                let message: String = a.get("message");
+                                context.error(
+                                    args,
+                                    EvalError::AssertionFailed(if message.is_empty() {
+                                        format!("{v}")
+                                    } else {
+                                        message
+                                    }),
+                                )?;
+                            }
+                        }
+                    }
+                    Err(err) => {
+                        // Called `assert` with no or more than 2 parameters
+                        context.error(args, err)?
+                    }
+                }
+
+                Ok(Value::None)
+            },
+            None,
+        )
+    }
+
+    fn all_equal<T: PartialEq + std::fmt::Debug>(mut iter: impl Iterator<Item = T>) -> bool {
+        if let Some(first) = iter.next() {
+            iter.all(|x| x == first)
+        } else {
+            true
+        }
+    }
+
+    pub fn assert_eq() -> Symbol {
+        Symbol::new_builtin_fn(
+            "assert_eq",
+            [
+                parameter!(array),                           // Parameter with any type
+                parameter!(message: String = String::new()), // Optional message
+            ]
+            .into_iter(),
+            &|params, args, context| {
+                match ArgumentMatch::find_multi_match(args, params) {
+                    Ok(multi_args) => {
+                        for array in multi_args.args {
+                            let array_value = &array.get_value("array").expect("missing parameter");
+
+                            if let Value::Array(exprs) = array_value {
+                                if !all_equal(exprs.iter()) {
+                                    let message: String = array.get("message");
+                                    context.error(
+                                        args,
+                                        EvalError::AssertionFailed(if message.is_empty() {
+                                            format!("Values differ: {exprs}")
+                                        } else {
+                                            "{message}".to_string()
+                                        }),
+                                    )?;
+                                }
+                            } else {
+                                let message: String = array.get("message");
+                                context.error(
+                                    args,
+                                    EvalError::AssertionFailed(if message.is_empty() {
+                                        format!("Invalid: {array_value}")
+                                    } else {
+                                        "{message}".to_string()
+                                    }),
+                                )?;
+                            }
+                        }
+                    }
+                    Err(err) => {
+                        // Called `assert` with no or more than 2 parameters
+                        context.error(args, err)?
+                    }
+                }
+
+                Ok(Value::None)
+            },
+            None,
+        )
+    }
+
+    pub fn assert_valid() -> Symbol {
+        Symbol::new_builtin_fn(
+            "assert_valid",
+            [
+                parameter!(target: Target),                  // Parameter name
+                parameter!(message: String = String::new()), // Optional message
+            ]
+            .into_iter(),
+            &|params, args, context| {
+                match ArgumentMatch::find_multi_match(args, params) {
+                    Ok(multi_args) => {
+                        for arg in multi_args.args {
+                            let target = arg.get::<Target>("target");
+                            if target.target.is_none() {
+                                context.error(
+                                    &arg,
+                                    EvalError::AssertionFailed(format!(
+                                        "Symbol `{}` not found.",
+                                        target.name
+                                    )),
+                                )?;
+                            }
+                        }
+                    }
+                    Err(err) => {
+                        // Called `assert` with no or more than 2 parameters
+                        context.error(args, err)?
+                    }
+                }
+
+                Ok(Value::None)
+            },
+            None,
+        )
+    }
+
+    pub fn assert_invalid() -> Symbol {
+        Symbol::new_builtin_fn(
+            "assert_invalid",
+            [
+                parameter!(target: Target),                  // Parameter name
+                parameter!(message: String = String::new()), // Optional message
+            ]
+            .into_iter(),
+            &|params, args, context| {
+                match ArgumentMatch::find_multi_match(args, params) {
+                    Ok(multi_args) => {
+                        for arg in multi_args.args {
+                            let target = arg.get::<Target>("target");
+                            if let Some(query) = target.target {
+                                log::trace!("target_name: {query}, {}", target.name);
+                                context.error(
+                                    &arg,
+                                    EvalError::AssertionFailed(format!(
+                                        "Found valid symbol '{name}' within module '{base}'.",
+                                        name = target.name,
+                                        base = query.base(&target.name)
+                                    )),
+                                )?;
+                            }
+                        }
+                    }
+                    Err(err) => {
+                        // Called `assert` with no or more than 2 parameters
+                        context.error(args, err)?
+                    }
+                }
+
+                Ok(Value::None)
+            },
+            None,
+        )
+    }
 }
--- crates/builtin/string.rs
+++ crates/builtin/string.rs
@@ -3,18 +3,18 @@
 
 //! µcad builtin string functions.
 
-use microcad_builtin_proc_macros::{builtin_fn, builtin_mod};
-use microcad_core::Integer;
-use microcad_lang::{diag::PushDiag, eval::EvalError, resolve::Symbol, value::Value};
+use microcad_builtin_proc_macros::builtin_mod;
 
 /// Module for built-in string functions.
 #[builtin_mod]
-pub fn string() {
-    [count];
-}
+pub mod string {
+    use microcad_builtin_proc_macros::builtin_fn;
+    use microcad_core::Integer;
+    use microcad_lang::{diag::PushDiag, eval::EvalError, resolve::Symbol, value::Value};
 
-/// Return the count of characters in a string.
-#[builtin_fn]
-fn count(s: String) -> Integer {
-    (s.chars().count() as Integer).into()
+    /// Return the count of characters in a string.
+    #[builtin_fn]
+    pub fn count(s: String) -> Integer {
+        (s.chars().count() as Integer).into()
+    }
 }
