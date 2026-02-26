92874d92d1b7b4a793120098612c0483cd73ebf9
Michael Winkelmann <michael@winkelmann.site>
2026-02-26 19:56:07 +0100

    color builtin_mod

--- crates/builtin/color.rs
+++ crates/builtin/color.rs
@@ -3,78 +3,76 @@
 
 //! Built-in color module `__builtin::color`.
 
-use microcad_core::{Color, Scalar};
-use microcad_lang::{parameter, resolve::*, value::*};
+use microcad_builtin_proc_macros::builtin_mod;
+use microcad_core::Color;
 
-/// Create a color from red, green and blue.
-fn rgb() -> Symbol {
-    Symbol::new_builtin_fn(
-        "rgb",
-        [
-            parameter!(r: Scalar),
-            parameter!(g: Scalar),
-            parameter!(b: Scalar),
-        ]
-        .into_iter(),
-        &|_params, args, _| {
-            Ok(Value::Tuple(Box::new(
-                Color::rgb(
+#[builtin_mod]
+pub mod color {
+    use microcad_core::Scalar;
+    use microcad_core::color::Color;
+    use microcad_lang::{parameter, resolve::*, value::*};
+
+    pub const RED: Color = Color::RED;
+    pub const BLUE: Color = Color::BLUE;
+    pub const GREEN: Color = Color::GREEN;
+    pub const YELLOW: Color = Color::YELLOW;
+    pub const CYAN: Color = Color::CYAN;
+    pub const MAGENTA: Color = Color::MAGENTA;
+    pub const BLACK: Color = Color::BLACK;
+    pub const WHITE: Color = Color::WHITE;
+    pub const GRAY: Color = Color::GRAY;
+    pub const ORANGE: Color = Color::ORANGE;
+    pub const PURPLE: Color = Color::PURPLE;
+    pub const PINK: Color = Color::PINK;
+    pub const BROWN: Color = Color::BROWN;
+    pub const LIME: Color = Color::LIME;
+    pub const TEAL: Color = Color::TEAL;
+    pub const NAVY: Color = Color::NAVY;
+    pub const TRANSPARENT: Color = Color::TRANSPARENT;
+
+    /// Create a color from red, green and blue.
+    pub fn rgb() -> Symbol {
+        Symbol::new_builtin_fn(
+            "rgb",
+            [
+                parameter!(r: Scalar),
+                parameter!(g: Scalar),
+                parameter!(b: Scalar),
+            ]
+            .into_iter(),
+            &|_params, args, _| {
+                Ok(Color::rgb(
                     args.get::<Scalar>("r") as f32,
                     args.get::<Scalar>("g") as f32,
                     args.get::<Scalar>("b") as f32,
                 )
-                .into(),
-            )))
-        },
-        None,
-    )
-}
+                .into())
+            },
+            None,
+        )
+    }
 
-/// Create a color from red, green, blue and alpha.
-fn rgba() -> Symbol {
-    Symbol::new_builtin_fn(
-        "rgba",
-        [
-            parameter!(r: Scalar),
-            parameter!(g: Scalar),
-            parameter!(b: Scalar),
-            parameter!(a: Scalar),
-        ]
-        .into_iter(),
-        &|_params, args, _| {
-            Ok(Color::rgba(
-                args.get::<Scalar>("r") as f32,
-                args.get::<Scalar>("g") as f32,
-                args.get::<Scalar>("b") as f32,
-                args.get::<Scalar>("a") as f32,
-            )
-            .into())
-        },
-        None,
-    )
-}
-
-/// Built-in color module.
-pub fn color() -> Symbol {
-    crate::ModuleBuilder::new("color")
-        .symbol(rgb())
-        .symbol(rgba())
-        .pub_const("RED", Color::RED)
-        .pub_const("BLUE", Color::BLUE)
-        .pub_const("GREEN", Color::GREEN)
-        .pub_const("YELLOW", Color::YELLOW)
-        .pub_const("CYAN", Color::CYAN)
-        .pub_const("MAGENTA", Color::MAGENTA)
-        .pub_const("BLACK", Color::BLACK)
-        .pub_const("WHITE", Color::WHITE)
-        .pub_const("GRAY", Color::GRAY)
-        .pub_const("ORANGE", Color::ORANGE)
-        .pub_const("PURPLE", Color::PURPLE)
-        .pub_const("PINK", Color::PINK)
-        .pub_const("BROWN", Color::BROWN)
-        .pub_const("LIME", Color::LIME)
-        .pub_const("TEAL", Color::TEAL)
-        .pub_const("NAVY", Color::NAVY)
-        .pub_const("TRANSPARENT", Color::TRANSPARENT)
-        .build()
+    /// Create a color from red, green, blue and alpha.
+    pub fn rgba() -> Symbol {
+        Symbol::new_builtin_fn(
+            "rgba",
+            [
+                parameter!(r: Scalar),
+                parameter!(g: Scalar),
+                parameter!(b: Scalar),
+                parameter!(a: Scalar),
+            ]
+            .into_iter(),
+            &|_params, args, _| {
+                Ok(Color::rgba(
+                    args.get::<Scalar>("r") as f32,
+                    args.get::<Scalar>("g") as f32,
+                    args.get::<Scalar>("b") as f32,
+                    args.get::<Scalar>("a") as f32,
+                )
+                .into())
+            },
+            None,
+        )
+    }
 }
