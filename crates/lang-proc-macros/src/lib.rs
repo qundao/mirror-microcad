7ef8e8ef6d0b97efd8602a17fe367063d0e9fe2c
Michael Winkelmann <michael@winkelmann.site>
2026-03-23 11:23:12 +0100

    derive_id proc macro

--- crates/lang-proc-macros/src/lib.rs
+++ crates/lang-proc-macros/src/lib.rs
@@ -10,13 +10,13 @@ use syn::*;
 #[proc_macro_derive(SrcReferrer)]
 pub fn derive_src_referrer(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
-    let name = input.ident.clone();
+    let name = &input.ident;
 
-    // parse fields, validate etc
-    // Only support structs with named fields
+    // Only support structs with named and unnamed fields.
     match &input.data {
         Data::Struct(ds) => match &ds.fields {
-            // Generate SrcReferrer for a struct with fields `struct Foo { bar: Integer, src_ref: SrcRef };`.
+            // Generate SrcReferrer for a struct with `src_ref` field:
+            // `struct Foo { bar: Integer, src_ref: SrcRef };`.
             Fields::Named(_) => {
                 quote! {
                     impl microcad_lang_base::SrcReferrer for #name {
@@ -26,6 +26,7 @@ pub fn derive_src_referrer(input: TokenStream) -> TokenStream {
                     }
                 }
             }
+            // Generate SrcReferrer for a tuple `struct Bar(Refer<Identifier>);`.
             Fields::Unnamed(_) => {
                 quote! {
                     impl microcad_lang_base::SrcReferrer for #name {
@@ -34,6 +35,37 @@ pub fn derive_src_referrer(input: TokenStream) -> TokenStream {
                         }
                     }
                 }
+            }
+            // Unit structs are not supported.
+            Fields::Unit => Error::new_spanned(&name, format!("Unit structs are not supported"))
+                .to_compile_error(),
+        },
+        _ => Error::new_spanned(&name, format!("Unions and enums are not supported"))
+            .to_compile_error(),
+    }
+    .into()
+}
+
+#[proc_macro_derive(Identifiable)]
+pub fn derive_id(input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as DeriveInput);
+    let name = input.ident.clone();
+
+    match &input.data {
+        Data::Struct(ds) => match &ds.fields {
+            // Generate `Identifiable` for a struct with `id` field `struct Foo { bar: Integer, id: Identifier };`.
+            Fields::Named(_) => {
+                quote! {
+                    impl crate::Identifiable for #name {
+                        fn id_ref(&self) -> &crate::Identifier  {
+                            &self.id
+                        }
+                    }
+                }
+            }
+            Fields::Unnamed(_) => {
+                Error::new_spanned(&name, format!("Unnamed structs are not supported"))
+                    .to_compile_error()
             }
             // Unit structs not supported.
             Fields::Unit => Error::new_spanned(&name, format!("Unit structs are not supported"))
@@ -44,11 +76,3 @@ pub fn derive_src_referrer(input: TokenStream) -> TokenStream {
     }
     .into()
 }
-
-/*
-#[proc_macro_derive(TreeDisplay)]
-pub fn derive_tree_display(input: TokenStream) -> TokenStream {}
-
-#[proc_macro_derive(Identifiable)]
-pub fn derive_id(input: TokenStream) -> TokenStream {}
-*/
