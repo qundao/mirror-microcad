002467458ff88c0fe4e85bb045d652db8381ff5e
Michael Winkelmann <michael@winkelmann.site>
2026-04-04 15:32:56 +0200

    cli: Use read_to_string

---
+++ crates/cli/commands/format.rs
@@ -0,0 +1,35 @@
+// Copyright © 2026 The µcad authors <info@microcad.xyz>
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+use miette::miette;
+
+use crate::{Cli, commands::RunCommand};
+
+/// Format a µcad file.
+#[derive(clap::Parser)]
+
+pub struct Format {
+    /// Input µcad file.
+    pub input: std::path::PathBuf,
+}
+
+impl RunCommand<()> for Format {
+    fn run(&self, _cli: &Cli) -> miette::Result<()> {
+        let source = std::fs::read_to_string(&self.input).map_err(|e| miette!("{e}"))?;
+        let tokens: Vec<_> = microcad_syntax::lex(&source).collect();
+        let source_file = microcad_syntax::parse(&tokens).map_err(|errors| {
+            miette!(
+                "{errors}",
+                errors = errors
+                    .iter()
+                    .map(|e| e.to_string())
+                    .collect::<Vec<_>>()
+                    .join("\n")
+            )
+        })?;
+
+        println!("{}", microcad_lang_format::format(&source_file));
+
+        Ok(())
+    }
+}
