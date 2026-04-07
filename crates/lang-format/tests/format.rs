734a6bbbbaa877d1296d99c552be37ffaf6c15b0
Michael Winkelmann <michael@winkelmann.site>
2026-04-07 16:56:39 +0200

    lang-format: Add test cases

--- crates/lang-format/tests/format.rs
+++ crates/lang-format/tests/format.rs
@@ -1,14 +1,33 @@
 // Copyright © 2026 The µcad authors <info@microcad.xyz>
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-macro_rules! test_case {
+// A test case whose format output does not change.
+macro_rules! formatted_test_case {
     ($name:ident) => {
         #[test]
         fn $name() {
             use microcad_lang_format::{FormatConfig, format_str};
             let name = stringify!($name);
-            let source = std::fs::read_to_string(format!("tests/test_cases/{name}.µcad"))
+            let source = std::fs::read_to_string(format!("tests/test_cases/formatted/{name}.µcad"))
                 .expect("No errors");
+            assert_eq!(
+                source,
+                format_str(&source, &FormatConfig::default()).expect("No errors")
+            );
+        }
+    };
+}
+
+// A test case whose format output does change.
+macro_rules! unformatted_test_case {
+    ($name:ident) => {
+        #[test]
+        fn $name() {
+            use microcad_lang_format::{FormatConfig, format_str};
+            let name = stringify!($name);
+            let source =
+                std::fs::read_to_string(format!("tests/test_cases/unformatted/{name}.µcad"))
+                    .expect("No errors");
             insta::assert_snapshot!(
                 name,
                 format_str(&source, &FormatConfig::default()).expect("No errors")
@@ -17,6 +36,4 @@ macro_rules! test_case {
     };
 }
 
-test_case!(workbench);
-//test_case!(builder);
-test_case!(extras);
+formatted_test_case!(array);
