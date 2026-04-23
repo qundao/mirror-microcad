cfc2f9a93da292fba510ed29ffc2c4c8a83ab81b
Michael Winkelmann <michael@winkelmann.site>
2026-04-23 21:08:13 +0200

    lang-format: unformatted tests

--- crates/lang-format/tests/format.rs
+++ crates/lang-format/tests/format.rs
@@ -1,22 +1,43 @@
 // Copyright © 2026 The µcad authors <info@microcad.xyz>
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
+fn _formatted_test_case(name: &str) {
+    use microcad_lang_format::{FormatConfig, format};
+    let source = std::fs::read_to_string(format!("tests/test_cases/formatted/{name}.µcad"))
+        .expect("No errors");
+    let ast = microcad_syntax::parse(&source).expect("No errors").ast;
+
+    pretty_assertions::assert_eq!(
+        source,
+        format(&ast, &FormatConfig::default()),
+        "Format error:\n{ast:#?}",
+    );
+}
+
+fn _unformatted_test_case(name: &str) {
+    use microcad_lang_format::{FormatConfig, format};
+    let source = std::fs::read_to_string(format!("tests/test_cases/unformatted/{name}.µcad"))
+        .expect("No errors");
+    let ast = microcad_syntax::parse(&source).expect("No errors").ast;
+    insta::assert_snapshot!(name, format(&ast, &FormatConfig::default()))
+}
+
 // A test case whose format output does not change.
 macro_rules! formatted_test_case {
     ($name:ident) => {
         #[test]
         fn $name() {
-            use microcad_lang_format::{FormatConfig, format};
-            let name = stringify!($name);
-            let source = std::fs::read_to_string(format!("tests/test_cases/formatted/{name}.µcad"))
-                .expect("No errors");
-            let ast = microcad_syntax::parse(&source).expect("No errors").ast;
+            _formatted_test_case(stringify!($name));
+        }
+    };
+}
 
-            pretty_assertions::assert_eq!(
-                source,
-                format(&ast, &FormatConfig::default()),
-                "Format error:\n{ast:#?}",
-            );
+// A test case whose format output does not change.
+macro_rules! unformatted_test_case {
+    ($name:ident) => {
+        #[test]
+        fn $name() {
+            _unformatted_test_case(stringify!($name));
         }
     };
 }
@@ -31,3 +52,5 @@ formatted_test_case!(statements);
 formatted_test_case!(workbench);
 formatted_test_case!(extras);
 formatted_test_case!(extras_module);
+
+unformatted_test_case!(init);
