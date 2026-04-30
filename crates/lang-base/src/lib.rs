841c15674f853f8aa937b9fbbe644db3f21e9b7e
Michael Winkelmann <michael@winkelmann.site>
2026-04-30 10:37:24 +0200

    lang-base: Small fixups for diag

--- crates/lang-base/src/lib.rs
+++ crates/lang-base/src/lib.rs
@@ -22,7 +22,8 @@ pub const MICROCAD_EXTENSIONS: &[&str] = &["µcad", "mcad", "ucad"];
 
 pub use code_display::*;
 pub use diag::{
-    Diag, DiagError, DiagHandler, DiagRenderOptions, DiagResult, Diagnostic, Level, PushDiag,
+    Diag, DiagError, DiagHandler, DiagList, DiagRenderOptions, DiagResult, Diagnostic, Level,
+    PushDiag,
 };
 pub use identifier::Identifier;
 pub use ord_map::{OrdMap, OrdMapValue};
