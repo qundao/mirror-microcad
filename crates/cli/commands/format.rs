518c6ea44d29085a1450949230a80599211d4565
Michael Winkelmann <michael@winkelmann.site>
2026-05-08 14:47:04 +0200

    cli: Remove format_md book

--- crates/cli/commands/format.rs
+++ crates/cli/commands/format.rs
@@ -11,35 +11,6 @@ pub struct Format {
     pub input: std::path::PathBuf,
 }
 
-/// High-level API to format an entire mdbook.
-pub fn format_mdbook(
-    mdbook: &mut microcad_lang_markdown::MdBook,
-    config: &FormatConfig,
-) -> Result<(), Diagnostics> {
-    let mut diagnostics = Diagnostics::default();
-
-    // 1. Iterate over code blocks. 'path' is the PathBuf of the .md file.
-    mdbook
-        .code_blocks_mut()
-        .filter(|(_, code_block)| code_block.can_format())
-        .for_each(|(path, code_block)| {
-            if let Err(err) = microcad_lang_format::format_str(&code_block.code, config) {
-                diagnostics.append(err);
-            } else if let Ok(formatted) = microcad_lang_format::format_str(&code_block.code, config)
-            {
-                // Only update the code if formatting succeeded
-                code_block.code = formatted;
-            }
-        });
-
-    // 3. If we hit issues, return the map in the specific variant
-    if diagnostics.has_errors() {
-        return Err(diagnostics);
-    }
-
-    Ok(())
-}
-
 impl RunCommand<()> for Format {
     fn run(&self, cli: &Cli) -> miette::Result<()> {
         let document = microcad_driver::Document::from_file_path(&self.input, cli.config.clone())?;
