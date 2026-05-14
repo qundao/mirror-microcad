3c8152c68ee2bce2e0f05015dcfb3e3ebe14496e
Michael Winkelmann <michael@winkelmann.site>
2026-05-14 14:12:36 +0200

    cli: remove use in format

--- crates/cli/commands/format.rs
+++ crates/cli/commands/format.rs
@@ -1,8 +1,6 @@
 // Copyright © 2026 The µcad authors <info@microcad.xyz>
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-use microcad_driver::{Document, locate};
-
 use crate::{Cli, commands::RunCommand};
 
 /// Format a µcad file.
@@ -16,7 +14,7 @@ pub struct Format {
 impl RunCommand<()> for Format {
     fn run(&self, cli: &Cli) -> miette::Result<()> {
         use microcad_driver::commands::{Format, FormatParameters, Sync, compile::Parse};
-        let mut document = Document::open(&self.input)?;
+        let mut document = microcad_driver::Document::open(&self.input)?;
         let params = FormatParameters::default();
 
         match document.parse().and(document.format(&params)) {
