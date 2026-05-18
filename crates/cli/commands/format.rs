bd5a76e8c1e890c5b0a5dc9705cf50faf389c5aa
Michael Winkelmann <michael@winkelmann.site>
2026-05-18 21:45:56 +0200

    cli: format via stdin

--- crates/cli/commands/format.rs
+++ crates/cli/commands/format.rs
@@ -7,31 +7,36 @@ use crate::{Cli, commands::RunCommand};
 #[derive(clap::Parser)]
 
 pub struct Format {
-    /// Input µcad file.
-    pub input: String,
+    /// Input µcad file if not set, we read from stdin.
+    pub input: Option<String>,
 }
 
 impl RunCommand<()> for Format {
     fn run(&self, cli: &Cli) -> miette::Result<()> {
         use microcad_driver::prelude as mu;
         use mu::traits::{Format, Parse, Sync};
-
-        let mut document = mu::Document::open(&self.input)?;
         let params = mu::FormatParameters::default();
 
-        match document.parse().and(document.format(&params)) {
-            Ok(true) => {
-                document.sync()?;
-                eprintln!("Formatted document.");
-            }
-            Ok(false) => {
-                eprintln!("Document has been already formatted.");
-            }
-            Err(_) => {
-                cli.print_diagnostics(&document);
-            }
-        }
+        match &self.input {
+            Some(input) => {
+                let mut document = mu::Document::open(&input)?;
 
-        Ok(())
+                match document.parse().and(document.format(&params)) {
+                    Ok(true) => {
+                        document.sync()?;
+                        eprintln!("Formatted document.");
+                    }
+                    Ok(false) => {
+                        eprintln!("Document has been already formatted.");
+                    }
+                    Err(_) => {
+                        cli.print_diagnostics(&document);
+                    }
+                }
+
+                Ok(())
+            }
+            None => mu::document::Stdin.format(&params).map(|_| ()),
+        }
     }
 }
