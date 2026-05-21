f102d0f547e83e2d498cb10702435469b5e4021b
Michael Winkelmann <michael@winkelmann.site>
2026-05-21 12:17:43 +0200

    lsp: test initialize

--- crates/lsp/tests/processor.rs
+++ crates/lsp/tests/processor.rs
@@ -1,2 +1,45 @@
-#[test]
-fn lsp_to_processor_request() {}
+//! Test the LSP processor
+
+use tower_lsp::{LanguageServer, lsp_types as lsp};
+
+use microcad_driver::prelude as mu;
+
+use microcad_lsp as mu_lsp;
+
+use googletest::prelude::*;
+
+/// check `lsp::InitializeParams` -> `lsp::InitializeResult`
+#[tokio::test]
+async fn initialize() -> Result<()> {
+    let config = mu::Config::default();
+
+    log::info!("Starting LSP server");
+
+    let processor = mu_lsp::processor::ProcessorInterface::run();
+
+    let (service, _) = tower_lsp::LspService::build(|client| {
+        mu_lsp::backend::Backend::new(client, processor, config.search_paths)
+    })
+    .custom_method(
+        "custom/activeFileChanged",
+        mu_lsp::backend::Backend::on_active_file_changed,
+    )
+    .finish();
+
+    let params = lsp::InitializeParams::default();
+
+    let backend = service.inner();
+
+    let init_result = backend.initialize(params).await;
+
+    assert_that!(
+        init_result,
+        ok(matches_pattern!(lsp::InitializeResult {
+            capabilities: matches_pattern!(lsp::ServerCapabilities {
+                document_formatting_provider: some(anything())
+            }),
+        }))
+    );
+
+    Ok(())
+}
