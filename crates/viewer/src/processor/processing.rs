50bbb1fdda0bc098b0c4ec20cfb1b115ca791078
Michael Winkelmann <michael@winkelmann.site>
2026-05-21 15:44:51 +0200

    viewer: fix missing Element::Value handling

--- crates/viewer/src/processor/processing.rs
+++ crates/viewer/src/processor/processing.rs
@@ -166,6 +166,7 @@ impl Processor {
         let model_ = model.borrow();
         // We only consider output geometries of workpieces and ignore the rest.
         let recurse = match model_.element() {
+            Value(_) => true, // Values might produce geometries at some point (to draw Vec2, Vec3 etc. in the scene)
             InputPlaceholder | Multiplicity | Group => true,
             Workpiece(_) | BuiltinWorkpiece(_) => {
                 let uuid = crate::processor::registry::generate_model_geometry_output_uuid(model);
