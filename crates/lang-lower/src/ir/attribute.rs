59dbe5eca9d7947ba998b83c344bb512654986b3
Michael Winkelmann <michael@winkelmann.site>
2026-06-10 17:25:29 +0200

    lang-lower: ir move doc_block to attribute

--- crates/lang-lower/src/ir/attribute.rs
+++ crates/lang-lower/src/ir/attribute.rs
@@ -4,7 +4,64 @@
 //! Attribute syntax entities.
 
 use crate::ir::{self, ConstantExpression};
-use microcad_lang_base::SrcRef;
+use microcad_lang_base::{Refer, SrcRef};
+
+use microcad_lang_proc_macros::SrcReferrer;
+
+/// Block of documentation comments, starting with `/// `.
+#[derive(Clone, Debug, Default, SrcReferrer)]
+pub struct DocBlock(pub Refer<Vec<String>>);
+
+impl DocBlock {
+    /// Create new doc block for builtin.
+    pub fn new_builtin(comment: &str) -> Self {
+        Self(Refer::none(
+            comment.lines().map(|s| format!("/// {s}")).collect(),
+        ))
+    }
+
+    /// Check if this doc block is empty.
+    pub fn is_empty(&self) -> bool {
+        self.0.is_empty()
+    }
+
+    /// Merge two doc blocks, e.g. for merging inner and outer docs
+    pub fn merge(a: &DocBlock, b: &DocBlock) -> DocBlock {
+        match (a.is_empty(), b.is_empty()) {
+            (true, true) => Self::default(),
+            (true, false) => b.clone(),
+            (false, true) => a.clone(),
+            _ => {
+                use microcad_lang_base::SrcReferrer;
+                let merged =
+                    a.0.iter()
+                        .chain([String::default()].iter()) // Add an empty line
+                        .chain(b.0.iter())
+                        .cloned()
+                        .collect::<Vec<_>>();
+                Self(Refer::new(
+                    merged,
+                    SrcRef::merge(&a.src_ref(), &b.src_ref()),
+                ))
+            }
+        }
+    }
+
+    /// Remove `///` comment marks and return each line as string.
+    pub fn fetch_lines(&self) -> Vec<String> {
+        self.0
+            .iter()
+            .filter_map(|s| s.strip_prefix("/// ").or(s.strip_prefix("///")))
+            .map(|s| s.trim_end().to_string())
+            .collect::<Vec<_>>()
+    }
+}
+
+impl std::fmt::Display for DocBlock {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", &self.0.value.join("\n"))
+    }
+}
 
 /// Metadata for a [`Model`]
 #[derive(Debug)]
