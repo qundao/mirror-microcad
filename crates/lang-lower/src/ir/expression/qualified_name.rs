a9981d8b289f5e2b0d94abd47908fa526c5bd472
Michael Winkelmann <michael@winkelmann.site>
2026-07-05 23:13:01 +0200

    lang-lower: Clean up ir::QualifiedName

--- crates/lang-lower/src/ir/expression/qualified_name.rs
+++ crates/lang-lower/src/ir/expression/qualified_name.rs
@@ -1,7 +1,7 @@
 // Copyright © 2024-2026 The µcad authors <info@microcad.xyz>
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-use derive_more::{Deref, DerefMut};
+use derive_more::Deref;
 
 use microcad_lang_base::{Identifier, Refer, SrcRef, SrcReferrer};
 use microcad_lang_proc_macros::SrcReferrer;
@@ -11,22 +11,8 @@ use serde::{Deserialize, Serialize};
 
 /// A *qualified name* consists of a list of *identifiers*, separated by `::`,
 /// e.g. `a::b::c`
-#[derive(
-    Default,
-    Clone,
-    Debug,
-    PartialEq,
-    Hash,
-    Eq,
-    Ord,
-    PartialOrd,
-    Deref,
-    DerefMut,
-    SrcReferrer,
-    Serialize,
-    Deserialize,
-)]
-pub struct QualifiedName(Refer<Vec<Identifier>>);
+#[derive(Default, Clone, Debug, PartialEq, Deref, SrcReferrer, Serialize, Deserialize)]
+pub struct QualifiedName(Refer<Box<[Identifier]>>);
 
 /// List of *qualified names* which can be displayed.
 #[derive(Debug, Deref)]
@@ -58,51 +44,12 @@ impl QualifiedName {
     /// - `ids`: *Identifiers* that concatenate to the *qualified name*.
     /// - `src_ref`: Reference for the whole name.
     pub fn new(ids: Vec<Identifier>, src_ref: SrcRef) -> Self {
-        Self(Refer::new(ids, src_ref))
-    }
-
-    /// Returns true if self is a qualified name with multiple ids in it
-    pub fn is_qualified(&self) -> bool {
-        self.0.len() > 1
+        Self(Refer::new(ids.into_boxed_slice(), src_ref))
     }
 
     /// Tells if self is in a specified module
     pub fn is_within(&self, module: &QualifiedName) -> bool {
         self.starts_with(module)
-    }
-
-    /// remove the first name from path
-    pub fn remove_first(&self) -> Self {
-        Self(Refer::new(self.0[1..].to_vec(), self.0.src_ref))
-    }
-
-    /// remove the first name from path
-    pub fn remove_last(self) -> Self {
-        Self(Refer::new(
-            self.0[..self.0.len() - 1].to_vec(),
-            self.0.src_ref,
-        ))
-    }
-
-    /// Append identifier to name
-    pub fn push(&mut self, id: Identifier) {
-        self.0.push(id)
-    }
-
-    /// Split name into first id and the rest
-    pub fn split_first(&self) -> (Identifier, QualifiedName) {
-        match self.len() {
-            0 => todo!("return None or error?"),
-            1 => (self.0[0].clone(), Self::default()),
-            _ => (self.0[0].clone(), Self(Refer::none(self.0[1..].into()))),
-        }
-    }
-
-    /// Add given prefix to name
-    pub fn with_prefix(&self, prefix: &QualifiedName) -> Self {
-        let mut full_name = prefix.clone();
-        full_name.append(&mut self.clone());
-        full_name
     }
 }
 
@@ -129,7 +76,7 @@ impl From<QualifiedName> for SourceSpan {
 impl From<Identifier> for QualifiedName {
     fn from(id: Identifier) -> Self {
         let src_ref = id.src_ref();
-        Self(Refer::new(vec![id], src_ref))
+        Self::new(vec![id], src_ref)
     }
 }
 
@@ -150,21 +97,9 @@ impl std::fmt::Display for QualifiedName {
     }
 }
 
-impl From<Refer<Vec<Identifier>>> for QualifiedName {
-    fn from(value: Refer<Vec<Identifier>>) -> Self {
-        Self(value)
-    }
-}
-
-impl FromIterator<Identifier> for QualifiedName {
-    fn from_iter<T: IntoIterator<Item = Identifier>>(iter: T) -> Self {
-        Self(Refer::none(iter.into_iter().collect()))
-    }
-}
-
 impl From<&Identifier> for QualifiedName {
     fn from(id: &Identifier) -> Self {
-        Self(Refer::none(vec![id.clone()]))
+        Self::new(vec![id.clone()], SrcRef::none())
     }
 }
 
