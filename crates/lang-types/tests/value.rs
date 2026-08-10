cdf9d32348ed7cc60732f0ed7728074f0bb16ca5
Michael Winkelmann <michael@winkelmann.site>
2026-08-10 18:02:32 +0200

    lang-types: Simplify value tests

--- crates/lang-types/tests/value.rs
+++ crates/lang-types/tests/value.rs
@@ -1,54 +1,46 @@
-use microcad_lang_types::{Integer, Scalar, Value, ValueResult};
+use microcad_lang_types::{Value, ValueResult};
 
-fn integer(value: i64) -> Value {
-    Integer::from_num(value).into()
-}
-
-fn scalar(value: f64) -> Value {
-    Scalar::from_num(value).into()
-}
-
-fn check(result: ValueResult, value: Value) {
+fn check(result: ValueResult, value: impl Into<Value>) {
     let result = result.expect("error result");
-    assert_eq!(result, value);
+    assert_eq!(result, value.into());
 }
 
 #[test]
 fn test_value_integer() {
-    let u = || integer(2);
-    let v = || integer(5);
-    let w = || scalar(5.0);
+    let u = || Value::from(2);
+    let v = || Value::from(5);
+    let w = || Value::from(5.0);
 
     // symmetric operations
-    check(u() + v(), integer(2 + 5));
-    check(u() - v(), integer(2 - 5));
-    check(u() * v(), integer(2 * 5));
-    check(u() / v(), scalar(2.0 / 5.0));
-    check(-u(), integer(-2));
+    check(u() + v(), 2 + 5);
+    check(u() - v(), 2 - 5);
+    check(u() * v(), 2 * 5);
+    check(u() / v(), 2.0 / 5.0);
+    check(-u(), -2);
 
     // asymmetric operations
-    check(u() + w(), scalar(2.0 + 5.0));
-    check(u() - w(), scalar(2.0 - 5.0));
-    check(u() * w(), scalar(2.0 * 5.0));
-    check(u() / w(), scalar(2.0 / 5.0));
+    check(u() + w(), 2.0 + 5.0);
+    check(u() - w(), 2.0 - 5.0);
+    check(u() * w(), 2.0 * 5.0);
+    check(u() / w(), 2.0 / 5.0);
 }
 
 #[test]
 fn test_value_scalar() {
-    let u = || scalar(2.0);
-    let v = || scalar(5.0);
-    let w = || integer(5);
+    let u = || Value::from(2.0);
+    let v = || Value::from(5.0);
+    let w = || Value::from(5);
 
     // symmetric operations
-    check(u() + v(), scalar(2.0 + 5.0));
-    check(u() - v(), scalar(2.0 - 5.0));
-    check(u() * v(), scalar(2.0 * 5.0));
-    check(u() / v(), scalar(2.0 / 5.0));
-    check(-u(), scalar(-2.0));
+    check(u() + v(), 2.0 + 5.0);
+    check(u() - v(), 2.0 - 5.0);
+    check(u() * v(), 2.0 * 5.0);
+    check(u() / v(), 2.0 / 5.0);
+    check(-u(), -2.0);
 
     // asymmetric operations
-    check(u() + w(), scalar(2.0 + 5.0));
-    check(u() - w(), scalar(2.0 - 5.0));
-    check(u() * w(), scalar(2.0 * 5.0));
-    check(u() / w(), scalar(2.0 / 5.0));
+    check(u() + w(), 2.0 + 5.0);
+    check(u() - w(), 2.0 - 5.0);
+    check(u() * w(), 2.0 * 5.0);
+    check(u() / w(), 2.0 / 5.0);
 }
