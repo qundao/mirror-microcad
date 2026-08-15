c6248139f9a08c0f14dcce56312ddca10304e498
Michael Winkelmann <michael@winkelmann.site>
2026-08-15 21:43:40 +0200

    lang-base: tree hash

--- crates/lang-base/src/tree/mod.rs
+++ crates/lang-base/src/tree/mod.rs
@@ -10,15 +10,30 @@ pub use indextree::*;
 
 pub use node_ref::{NodeMut, NodeRef};
 
+use serde::{Deserialize, Serialize};
 pub use tree_display::{FormatTree, TreeDisplay, TreeState};
 
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
 pub struct Tree<T> {
     root: NodeId,
     arena: Arena<T>,
 }
 
 impl<T> Tree<T> {
+    pub fn new(root: NodeId, arena: Arena<T>) -> Self {
+        Self { root, arena }
+    }
+
     pub fn root<'a>(&'a self) -> NodeRef<'a, T> {
         NodeRef::new(self.root, &self.arena)
+    }
+}
+
+impl<T> std::hash::Hash for Tree<T>
+where
+    T: std::hash::Hash,
+{
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        self.root().descendants().for_each(|node| node.hash(state));
     }
 }
