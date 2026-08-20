16b1cc0958556158390982e9353e241e0d491468
Michael Winkelmann <michael@winkelmann.site>
2026-08-20 14:10:47 +0200

    lang-base: Remove impl_tree_types macro

--- crates/lang-base/src/tree/mod.rs
+++ crates/lang-base/src/tree/mod.rs
@@ -11,26 +11,3 @@ pub use indextree::*;
 pub use node_ref::{NodeMut, NodeRef};
 
 pub use tree_display::{FormatTree, TreeDisplay, TreeState};
-
-#[macro_export]
-macro_rules! impl_tree_types {
-    ($vis:vis $tree:ident<$item:ty>) => {
-        impl $tree {
-            pub fn new(root: $item) -> Self {
-                let mut arena = Arena::default();
-                let root = arena.new_node(root);
-                Self { root, arena }
-            }
-
-            pub fn root<'a>(&'a self) -> NodeRef<'a> {
-                NodeRef::new(self.root, &self.arena)
-            }
-        }
-
-        $vis type Arena = $crate::tree::Arena<$item>;
-        $vis type Node = $crate::tree::Node<$item>;
-        $vis type NodeRef<'a> = $crate::tree::NodeRef<'a, $item>;
-        $vis type NodeMut<'a> = $crate::tree::NodeMut<'a, $item>;
-        $vis type NodeId = $crate::tree::NodeId;
-    };
-}
