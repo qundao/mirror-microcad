c9fcc14f2b66e8a08b1a92e559ddf6a0ea9acbb3
Michael Winkelmann <michael@winkelmann.site>
2026-09-01 21:49:34 +0200

    lang-lower: Improve Fold

--- crates/lang-lower/src/fold.rs
+++ crates/lang-lower/src/fold.rs
@@ -1,17 +1,15 @@
 // Copyright © 2024-2026 The µcad authors <info@microcad.xyz>
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-//! Constant fold.
+//! Fold constant values.
 
 use microcad_builtin::BuiltinItem;
 use microcad_lang_base::SymbolId;
-use microcad_lang_types::Arguments;
+use microcad_lang_types::{Arguments, Value};
 
-use crate::{
-    LowerContext,
-    ir::{self, ConstantValue},
-};
+use crate::{LowerContext, ir};
 
+/// A v
 pub struct Fold<'a, 'source> {
     context: &'a mut LowerContext<'source>,
 }
@@ -19,6 +17,50 @@ pub struct Fold<'a, 'source> {
 impl<'a, 'source> Fold<'a, 'source> {
     pub fn new(context: &'a mut LowerContext<'source>) -> Self {
         Self { context }
+    }
+
+    fn path_to_value(&mut self, path: &mut ir::Path) -> Option<Value> {
+        match path.symbol_id() {
+            Some(SymbolId::Builtin(builtin_id)) => match self.context.builtins.get(*builtin_id) {
+                Some(BuiltinItem::Constant(constant)) => Some(constant.value()),
+                _ => None,
+            },
+            _ => None,
+        }
+    }
+
+    fn call_to_value<Expr: ir::ExprSpec, F>(
+        &mut self,
+        call: &mut ir::Call<Expr>,
+        mut f: F,
+    ) -> Option<Value>
+    where
+        F: FnMut(&mut Fold<'a, 'source>, &mut Expr),
+    {
+        call.args.args.iter_mut().for_each(|arg| match arg {
+            ir::Argument::Unnamed(expr) => f(self, expr),
+            ir::Argument::Named { expr, .. } | ir::Argument::AutoNamed { expr, .. } => {
+                f(self, expr);
+            }
+        });
+
+        let builtin_fn = match &call.path.symbol_id() {
+            Some(microcad_lang_base::SymbolId::Builtin(builtin_id)) => {
+                match self.context.builtins.get(*builtin_id) {
+                    Some(BuiltinItem::Function(builtin_fn)) => builtin_fn,
+                    _ => return None,
+                }
+            }
+            _ => return None,
+        };
+
+        match call.args.into_arguments() {
+            Some(arguments) => match builtin_fn.call_isolated(arguments) {
+                Ok(value) => Some(value),
+                Err(_) => None,
+            },
+            None => None,
+        }
     }
 }
 
@@ -50,47 +92,20 @@ impl<'a, 'source> ir::visitor::ConstantVisitorMut for Fold<'a, 'source> {
             ir::ConstantExpression::Invalid | ir::ConstantExpression::Value(_) => {}
 
             // Fold builtin constants into values
-            ir::ConstantExpression::Path(path) => match path.symbol_id() {
-                Some(SymbolId::Builtin(builtin_id)) => {
-                    match self.context.builtins.get(*builtin_id) {
-                        Some(BuiltinItem::Constant(constant)) => {
-                            *expr = ir::ConstantExpression::Value(ConstantValue::from_value(
-                                constant.value(),
-                            ));
-                        }
-                        _ => {}
-                    }
+            ir::ConstantExpression::Path(path) => match self.path_to_value(path) {
+                Some(value) => {
+                    *expr = ir::ConstantExpression::Value(ir::ConstantValue::from_value(value));
                 }
-                _ => {}
+                None => {}
             },
 
             // Fold built-in calls
             ir::ConstantExpression::Call(call) => {
-                call.args.args.iter_mut().for_each(|arg| match arg {
-                    ir::Argument::Unnamed(expr) => self.visit_constant_expr(expr),
-                    ir::Argument::Named { expr, .. } | ir::Argument::AutoNamed { expr, .. } => {
-                        self.visit_constant_expr(expr);
+                match self.call_to_value(call, |fold, expr| fold.visit_constant_expr(expr)) {
+                    Some(value) => {
+                        *expr = ir::ConstantExpression::Value(ir::ConstantValue::from_value(value));
                     }
-                });
-
-                let builtin_fn = match &call.path.symbol_id() {
-                    Some(microcad_lang_base::SymbolId::Builtin(builtin_id)) => {
-                        match self.context.builtins.get(*builtin_id) {
-                            Some(BuiltinItem::Function(builtin_fn)) => builtin_fn,
-                            _ => return,
-                        }
-                    }
-                    _ => return,
-                };
-
-                match call.args.into_arguments() {
-                    Some(arguments) => match builtin_fn.call_isolated(arguments) {
-                        Ok(value) => {
-                            *expr = ir::ConstantExpression::Value(ConstantValue::from_value(value))
-                        }
-                        Err(_) => return,
-                    },
-                    None => return,
+                    None => {}
                 }
             }
         }
