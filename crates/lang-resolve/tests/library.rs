420f70553263ded14114a01aa41c50392deb4e4e
Michael Winkelmann <michael@winkelmann.site>
2026-09-08 19:19:43 +0200

    lang-resolve: Test Library::look_up
    Signed-off-by: Michael Winkelmann <michael@winkelmann.site>

--- crates/lang-resolve/tests/library.rs
+++ crates/lang-resolve/tests/library.rs
@@ -3,8 +3,11 @@
 
 //! Library tests
 
+use std::str::FromStr;
+
 use microcad_lang_base::{DiagRenderOptions, Diagnostics};
-use microcad_lang_resolve::ResolveContext;
+use microcad_lang_lower::ir::UnresolvedPath;
+use microcad_lang_resolve::{ResolveContext, SymbolNodeExt};
 
 fn ctx() -> ResolveContext {
     let mut ctx = ResolveContext::new();
@@ -13,11 +16,43 @@ fn ctx() -> ResolveContext {
     ctx
 }
 
+fn path(name: &str) -> UnresolvedPath {
+    UnresolvedPath::from_str(name).expect("A valid path")
+}
+
 #[test]
 fn load_source_inline_module() {
     let context = ctx();
     match ctx().load_file("tests/test_cases/inline_module.µcad") {
-        Ok(library) => insta::assert_snapshot!("load_source_inline_module", library),
+        Ok(library) => {
+            for child in library.root().children() {
+                println!("{child}");
+            }
+
+            let inline_module = library.root().find_child("inline_module").expect("A child");
+
+            let b = library
+                .look_up(inline_module, &path("b"))
+                .expect("Add node");
+            library
+                .look_up(b, &path("::inline_module"))
+                .expect("An inline module");
+
+            let d = library
+                .look_up(b, &path("::inline_module::b::d"))
+                .expect("An inline module 'd'");
+
+            let c = library
+                .look_up(b, &path("::inline_module::b::C"))
+                .expect("A constant 'C'");
+
+            // Search `b` in `d`
+            let b = library
+                .look_up(d, &path("b"))
+                .expect("An inline module 'b'");
+
+            insta::assert_snapshot!("load_source_inline_module", library)
+        }
         Err(err) => {
             let diagnostics = Diagnostics::from(context.diag);
 
